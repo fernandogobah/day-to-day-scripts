edf707649dcd430e861ae3fcd91cadf51c583d43
fernandogobah <fernandogobah@gmail.com>
2022-04-07 15:20:04 -0300

    update

--- message/send_telegram/msg_today_telegram.sh
+++ message/send_telegram/msg_today_telegram.sh
@@ -8,4 +8,4 @@ sleep $[ ( $RANDOM % 600 ) + 1 ]s
 to=My_Home_🏡_☀️
 curl -s https://www.bibliaon.com/versiculo_do_dia/ | html2text -width 500 | grep -A3 "Hoje" > $HOME/msg_do_dia.txt     
 
-(echo "contact_list" ; sleep 5 ; echo "send_text $to $HOME/msg_do_dia.txt" ; echo "safe_quit") | telegram-cli --enable-msg-id
+(echo "dialog_list" ; sleep 5 ; echo "send_text $to $HOME/msg_do_dia.txt" ; echo "safe_quit") | telegram-cli --enable-msg-id
