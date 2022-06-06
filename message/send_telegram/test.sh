40b34c76d4dc66da2fedff014fae2cce43d6de7c
fernandogobah <fernandogobah@gmail.com>
2022-06-06 13:25:22 -0300

    meshagent

---
+++ message/send_telegram/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Author: Fernando Lima
+# E-mail: fernandogobah@gmail.com
+# Last Update: 2022-04-07
+# Desc: Script to send message in telegram
+
+sleep $[ ( $RANDOM % 600 ) + 1 ]s
+to=My_Home_🏡_☀️
+curl -s https://www.bibliaon.com/versiculo_do_dia/ | html2text -width 500 | grep -A3 "Hoje" > $HOME/msg_do_dia.txt     
+
+(echo "dialog_list" ; sleep 5 ; echo "send_text $to $HOME/msg_do_dia.txt" ; echo "safe_quit") | telegram-cli --enable-msg-id
